70472f548ca5d9abb2910e2ff7b1754e2d8fd5eb
s4mma3l <lineartnw17@aol.com>
2025-10-22 09:45:22 -0600

    correccion de direccion

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,31 @@
 // frontend/next.config.js
 /** @type {import('next').NextConfig} */
+
+// Define el nombre de tu repositorio aquí
+const repoName = 'https://s4mma3l.github.io/FortalezaDigitalCR/'; // <-- CAMBIA ESTO AL NOMBRE EXACTO DE TU REPO
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '/';
+let basePath = '';
+
+if (isGithubActions) {
+  // Trim slash if mapRepo exists, and leading slash if not
+  // assetPrefix = `/${repoName}/` // Originalmente así, pero puede causar doble slash
+  // basePath = `/${repoName}` // Originalmente así
+  assetPrefix = `/${repoName}`; // Asegurar un solo slash inicial
+  basePath = `/${repoName}`; // Asegurar un solo slash inicial
+}
+
+
 const nextConfig = {
   output: 'export', // Habilita la exportación estática
-  // DESCOMENTA Y AJUSTA si tu repo NO es tu-usuario.github.io
-  // basePath: '/fortaleza-digital-cr', // Nombre de tu repositorio
-  // assetPrefix: '/fortaleza-digital-cr/', // Nombre de tu repositorio con /
+  // Configurar basePath y assetPrefix para GitHub Pages
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true, // Necesario para 'next export'
   },
 }
+
 module.exports = nextConfig
